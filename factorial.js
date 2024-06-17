cb321171799e34f0f56215b6792934873cf49124
Prathamesh Surve <91028092+PrathameshSurve@users.noreply.github.com>
2024-06-17 10:55:33 +0530

    find the factorial of number.

---
+++ factorial.js
@@ -0,0 +1,13 @@
+function factorial(number) { 
+
+  if (number === 0 || number === 1) { 
+    return 1; 
+
+  } else { 
+    return number * factorial(number - 1); 
+
+  } 
+
+} 
+
+console.log(factorial(4));
